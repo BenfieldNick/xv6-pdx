2fe76efe3e13df696fdcf37cf68586c8e561ee91
Kevin Loftis <loftiskg@mail.lipscomb.edu>
2018-05-16 22:02:58 -0700

    added file that should have been added

---
+++ waittest.c
@@ -0,0 +1,31 @@
+#ifdef CS333_P3P4
+
+#include "user.h"
+
+int
+main(){
+  int pid = fork();
+  if(pid < 0){
+    printf(1,"Fork Error\n");
+    exit();
+  }
+  if(pid == 0){
+    printf(1, "child exits\n");
+    exit();
+  }
+  printf(1,"Parent Sleeping for 10 seconds\n");
+  sleep(10000);
+  printf(1,"Parent calling wait\n");
+  wait();
+  printf(1,"Parent sleeping for 10 seconds\n");
+  sleep(10000);
+
+  printf(1,"Parent exiting\n");
+  exit();
+
+}
+
+
+
+
+#endif
